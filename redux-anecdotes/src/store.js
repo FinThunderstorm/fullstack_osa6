80b64bcdfff7134aee1e4cbcbce1d0454ae4672f
Tuomas Alanen <tuomas@paaryna.fi>
2020-03-25 09:51:19 +0200

    6.9

---
+++ redux-anecdotes/src/store.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from 'redux'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+})
+
+const store = createStore(reducer, composeWithDevTools())
+
+export default store
